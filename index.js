7adc12fe0febf4a7afae3281b45092481ae36b53
ss96 <srikanthjoshi96@gmail.com>
2024-01-29 01:39:16 +0530

    bulbproject

---
+++ index.js
@@ -0,0 +1,15 @@
+function bulbon(){
+    
+    document.getElementById("changestatement").innerHTML="THE BULB IS ON!"
+    document.getElementById("myimg").src="images/bulbglowing4.webp.png"
+    
+    
+    
+}
+
+function bulboff(){
+    document.getElementById("changestatement").innerHTML="THE BULB IS OFF"
+    document.getElementById("myimg").src="images/bulbnotglowing4.jpeg.png"
+
+    
+}
